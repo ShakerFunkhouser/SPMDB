c15cc9bfdb5451a6be84d2f3fb6d3e4cfa86830e
theVTOQ <vonfunkhouserprod@gmail.com>
2021-01-08 14:22:19 -0700

    Copy respository from Gitlab.

---
+++ src/main/java/com/revature/data/GenericDAO.java
@@ -0,0 +1,18 @@
+package com.revature.data;
+
+import java.util.Set;
+
+public interface GenericDAO<T> {
+	//create:
+	Integer add(T t);
+	
+	//read:
+	Set<T> getAll();
+	T getById(Integer id);
+	
+	//update:
+	void update(T t);
+	
+	//delete:
+	void delete(T t);
+}
